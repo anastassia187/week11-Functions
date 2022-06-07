8983b842ed506637c4dd464ba541a8566cbaeb0d
Anastassia Kuhartsik <nastjonka187@gmail.com>
2022-06-08 02:20:41 +0300

    week11

---
+++ CelsiusFahrenheit/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CelsiusFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            Console.WriteLine("Enter air temperature in Celsius");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("In Fahrenheit = " + Converting(temp));
+            Console.ReadKey();
+
+            Console.WriteLine("Enter air temperature in Fahrenheit");
+            double temp1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine( "In Celsius = " + ConvertingInF(temp1));
+            Console.ReadKey();
+
+        }
+
+       public static double Converting(double temp)
+        {
+
+
+            double result = temp * (9 / 5) + 32;
+            return result;
+        }
+
+       public  static double ConvertingInF(double temp1)
+        {
+            double result = (temp1 - 32) * 5 / 9;
+            return result;
+        }
+
+
+
+
+    }
+}
+  
